9e6030aec7f713e9bc0752974a83ba26f47e55e7
SoinRoma <soinroma26@gmail.com>
2022-12-26 13:20:05 +0500

    - added task for html and updates scripts for start gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,12 @@ const { src, dest } = require("gulp");
 const sass = require("gulp-sass");
 const csso = require("gulp-csso");
 const htmlmin = require("gulp-htmlmin");
-const del = require("del");
 const sync = require("browser-sync").create();
+
+function html() {
+  return src("src/**.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest("dist"));
+}
+
+exports.html = html;
