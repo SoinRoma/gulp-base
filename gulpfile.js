2130994dc19b5eb60a2331359af91e69d621bbad
SoinRoma <soinroma26@gmail.com>
2022-12-26 14:54:48 +0500

    - added task for css

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
-const { src, dest } = require("gulp");
-const sass = require("gulp-sass");
-const csso = require("gulp-csso");
+const { src, dest, series } = require("gulp");
 const htmlmin = require("gulp-htmlmin");
+const sass = require("gulp-sass")(require("sass"));
+const csso = require("gulp-csso");
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
 const sync = require("browser-sync").create();
 
 function html() {
@@ -10,4 +12,18 @@ function html() {
     .pipe(dest("dist"));
 }
 
+function scss() {
+  return src("src/scss/**.scss")
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 2 versions"],
+      })
+    )
+    .pipe(csso())
+    .pipe(concat("index.css"))
+    .pipe(dest("dist"));
+}
+
 exports.html = html;
+exports.scss = scss;
