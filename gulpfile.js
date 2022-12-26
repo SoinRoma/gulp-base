1531aa5dc354d840c476ba858c45db0f9030b0c4
SoinRoma <soinroma26@gmail.com>
2022-12-26 15:05:28 +0500

    -added task for build and server

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-const { src, dest, series } = require("gulp");
+const { src, dest, series, watch } = require("gulp");
 const htmlmin = require("gulp-htmlmin");
 const sass = require("gulp-sass")(require("sass"));
 const csso = require("gulp-csso");
 const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
 const sync = require("browser-sync").create();
+const del = require("del");
 
 function html() {
   return src("src/**.html")
@@ -25,5 +26,19 @@ function scss() {
     .pipe(dest("dist"));
 }
 
-exports.html = html;
-exports.scss = scss;
+function clear() {
+  return del("dist");
+}
+
+function dev() {
+  sync.init({
+    server: "./dist",
+  });
+
+  watch("src/**.htnl", series(html)).on("change", sync.reload);
+  watch("src/scss/**.scss", series(scss)).on("change", sync.reload);
+}
+
+exports.build = series(clear, scss, html);
+exports.dev = series(clear, scss, html, dev);
+exports.clear = clear;
